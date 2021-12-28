f0aa976e6dd89ee1dc291604258aa08048722046
xiangchengkang <2043766637@qq.com>
2021-12-28 16:06:54 +0800

    hotfix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello 2!");
+        System.out.println("hello 4 by hhot fix");
 
     }
 }
